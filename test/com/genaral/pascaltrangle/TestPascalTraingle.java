bb9434fcab6f7394939e4adc5f7d01d1609d4471
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2026-02-13 11:54:59 +0100

    Update TestPascalTraingle.java

--- test/com/genaral/pascaltrangle/TestPascalTraingle.java
+++ test/com/genaral/pascaltrangle/TestPascalTraingle.java
@@ -42,4 +42,30 @@ public class TestPascalTraingle {
 		assertEquals(PascalTraingle.valueInTrangle(6, 3), expectedValueAt_63);
 
 	}
+
+	@Test
+	public void shouldReturnValueInPascalTriangleSecond() {
+
+		int expectedValueAt_00 = 1;
+		String expectedValueAt_01 = "Error Index";
+		int expectedValueAt_10 = 1;
+		int expectedValueAt_11 = 1;
+		int expectedValueAt_22 = 1;
+		int expectedValueAt_40 = 1;
+		int expectedValueAt_63 = 20;
+
+		assertEquals(PascalTraingle.valueInTrangle(0, 0), expectedValueAt_00);
+
+		try {
+			PascalTraingle.valueInTrangle(0, 1);
+		} catch (RuntimeException e) {
+			assertEquals(e.getMessage(), expectedValueAt_01);
+		}
+		assertEquals(PascalTraingle.valueInTrangle(1, 3), expectedValueAt_10);
+		assertEquals(PascalTraingle.valueInTrangle(1, 2), expectedValueAt_11);
+		assertEquals(PascalTraingle.valueInTrangle(2, 2), expectedValueAt_22);
+		assertEquals(PascalTraingle.valueInTrangle(4, 0), expectedValueAt_40);
+		assertEquals(PascalTraingle.valueInTrangle(6, 3), expectedValueAt_63);
+
+	}
 }
